592f77fc3cea3da675edd5e8f316652b435375c9
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-27 09:25:11 +0300

    integrate react-router-dom dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { motion } from "framer-motion"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <div className="m-20">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 2 }}
-          className="w-[100px] h-[100px] bg-blue-500 rounded-full"
-        />
-      </div>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/details" element={<h1>Details</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
