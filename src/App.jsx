b32c17a203c5d3a6824b42b9270357773ee32455
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-27 09:31:06 +0300

    introduce Home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import Home from "./components/pages/Home"
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/details" element={<h1>Details</h1>} />
       </Routes>
     </Router>
