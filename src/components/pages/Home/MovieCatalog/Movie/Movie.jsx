14901f1c5bc278aa068ef16860c2f375872dc074
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-28 15:32:53 +0300

    visualize an animated overlay on a given movie when it is marked as a favorite

--- src/components/pages/Home/MovieCatalog/Movie/Movie.jsx
+++ src/components/pages/Home/MovieCatalog/Movie/Movie.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
+
 import { motion } from "framer-motion"
 
 const Movie = ({ movie: { image, title } }) => {
+  const [isFavorite, setFavorite] = useState(false)
+
+  const handleMarkAsFavorite = () => {
+    setFavorite(!isFavorite)
+  }
+
   return (
     <div className="flex">
       <div className="text-center">
-        <motion.div whileHover={{ scale: 1.1 }} className="m-8 mb-0 md:w-[160px] lg:w-[220px]">
-          <img src={`images/${image}`} className="rounded-lg min-h-[350px]" />
+        <motion.div whileHover={{ scale: 1.1 }} className="relative m-8 mb-0 md:w-[160px] lg:w-[220px]">
+          {
+            isFavorite && (
+              <motion.div
+                onAnimationComplete={() => setFavorite(!isFavorite)}
+                initial={{ opacity: "0%" }}
+                animate={{ opacity: "100%" }}
+                transition={{ duration: 2 }}
+                className="absolute w-full bg-white rounded-lg h-[350px] flex flex-col justify-center items-center text-green-600"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-14">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                <p className="font-bold mt-5">Added as a Favorite</p>
+              </motion.div>
+            )
+          }
+          <img src={`images/${image}`} className="rounded-lg h-[350px]" />
           <motion.p
             className="text-xl text-white h-[90px] mt-2"
             initial={{ opacity: 0 }} animate={{ opacity: "100%" }} transition={{ duration: 1, delay: 4 }}
@@ -14,6 +38,17 @@ const Movie = ({ movie: { image, title } }) => {
           </motion.p>
         </motion.div>
       </div>
+      {
+        isFavorite ? (
+          <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-white mt-8 cursor-pointer">
+            <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+          </motion.svg>
+        ) : (
+          <motion.svg initial={{ opacity: 0 }} animate={{ opacity: "100%" }} transition={{ duration: 1, delay: 4 }} onClick={handleMarkAsFavorite} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white mt-8 cursor-pointer">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+          </motion.svg>
+        )
+      }
     </div>
   )
 }
