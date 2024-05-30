11e195dec2a7023fbdd76f8d45efff8100abccc9
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-30 11:02:42 +0300

    change the color of the cinema label in the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ import filmRoll from "/images/film-roll.png"
 
 const Header = () => (
   <div className="flex justify-between items-center bg-blue-500 h-full p-2">
-    <div className="ms-5 w-[200px] flex items-center">
+    <div className="ms-5 flex items-center">
       <img src={filmRoll} className="w-[40px]" />
-      <h1 className="ms-3 text-2xl font-bold text-white">Cinema</h1>
+      <h1 className="ms-3 text-2xl font-bold text-blue-900">Cinema</h1>
     </div>
 
     <div className="me-5">
