5f1ff89c053f46619350f89240d9124447d17325
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-31 13:24:47 +0300

    improve the header menu visualization under smartphone resolution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,16 +11,16 @@ const Header = () => {
   ]
 
   return (
-    <div className="flex justify-between items-center bg-blue-500 h-full p-2">
+    <div className="flex flex-col md:flex-row justify-between items-center bg-blue-500 h-full p-2">
       <Link to="/" className="ms-5 flex items-center">
         <img src={filmRoll} className="w-[40px]" />
         <h1 className="ms-3 text-2xl font-bold text-blue-900">Cinema</h1>
       </Link>
 
-      <div className="me-5">
+      <div className="m-5 grid grid-rows-2 grid-cols-2 gap-2 md:grid-flow-col md:grid-rows-1">
         {
           links.map((v, i) => (
-            <Link key={i} to={v.to} className={`text-white hover:text-white hover:underline me-5 ${v.active ? "bg-blue-800 p-2 rounded-full" : "bg-transparent p-2"}`}>{v.label}</Link>
+            <Link key={i} to={v.to} className={`text-white text-center hover:text-white hover:underline me-5 ${v.active ? "bg-blue-800 p-2 rounded-full" : "bg-transparent p-2"}`}>{v.label}</Link>
           ))
         }
       </div>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 const Layout = ({ header, children }) => (
   <div className="flex flex-col h-screen">
-    <div className="h-[60px]">{header}</div>
+    <div className="lg:h-[60px]">{header}</div>
     <div className="h-full">{children}</div>
   </div>
 )
