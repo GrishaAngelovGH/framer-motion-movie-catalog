194d08801631eeb860b25c75d045a5520bd5b081
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-02 13:18:07 +0300

    integrate ComboBox into the Experiences page

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 
 const ProductCard = ({ image, title, content }) => (
-  <motion.div whileHover={{ scale: 1.1 }} className="bg-white p-2 min-h-[400px] rounded-md w-2/3 text-center">
+  <motion.div whileHover={{ scale: 1.1 }} className="min-w-[300px] bg-white p-2 min-h-[400px] rounded-md w-2/3 text-center">
     <img src={image} className="inline-block w-full rounded-md" />
     <p className="m-5 text-xl">{title}</p>
     <p className="text-gray-500">{content}</p>
--- src/components/pages/Experiences/Experiences.jsx
+++ src/components/pages/Experiences/Experiences.jsx
@@ -3,18 +3,25 @@ import { motion } from "framer-motion"
 import LayoutPage from "components/pages/LayoutPage"
 import Carousel from "components/Carousel"
 import ProductCard from "components/ProductCard"
+import ComboBox from "components/ComboBox"
 
 const Experiences = () => {
   const slides = [
     { image: "/images/hotdog-wedges.jpg", paragraphs: ["Enjoy a film feast", "Nothing beats watching a great film while tucking into an equally-great meal. Skip the restaurant, head to the cinema and order a delicious feast to take into the cinema. Try our chicken strips, nachos or new mozzarella sticks."] },
     { image: "/images/popcorn.jpg", paragraphs: ["Get the king of cinema snacks", "Popcorn and film, they go together like Batman and Robin or Woody and Buzz. That’s why we make sure ours is the best."] },
-    { image: "/images/ice-cream.jpg", paragraphs: ["Treat yourself", "Fancy something indulgent? You’re spoiled for choice at the cinema. Order Ben &amp; Jerry’s Chocolate Fudge Brownie, Caramel Chew Chew or a dozen other flavours. Sip a cappuccino from Costa or mix it up with a sugar-free Tango Ice Blast."] }
+    { image: "/images/ice-cream.jpg", paragraphs: ["Treat yourself", "Fancy something indulgent? You’re spoiled for choice at the cinema. Order Ben & Jerry’s Chocolate Fudge Brownie, Caramel Chew Chew or a dozen other flavours. Sip a cappuccino from Costa or mix it up with a sugar-free Tango Ice Blast."] }
   ]
 
   const favoriteProducts = [
     { image: "/images/kidscombo.jpg", title: "Kids Mix", content: "Get great value on snacks for your little ones with our Kids Mix combo, featuring a small popcorn, two snacks and a delicious drink for just £4.50." },
     { image: "/images/popcorn-and-coke.jpg", title: "Popcorn and Coca-Cola", content: "Going to the cinema wouldn’t be the same without popcorn. We make sure ours is perfectly popped for freshness. To wash it down, nothing beats an ice-cold, refreshing Coca-Cola. Order the dream team of cinema treats." },
     { image: "/images/ice-blast.jpg", title: "Tango Ice Blast", content: "Feel the frozen explosion with sugar-free Tango Ice Blast. It’s the guilt-free way to satisfy your sweet tooth. Order delicious slushy flavours like cherry, raspberry or a mix of both and let the icy zing dance on your tongue." }
+  ]
+
+  const comboBoxes = [
+    [{ image: "/images/combo-coca-cola.jpg", title: "Coca-Cola", price: 3 }, { image: "/images/combo-chips.jpg", title: "Chips", price: 7 }, { image: "/images/combo-ice-cream.jpg", title: "Ice-Cream", price: 10 }],
+    [{ image: "/images/combo-fanta.jpg", title: "Fanta", price: 3 }, { image: "/images/combo-pizza.jpg", title: "Pizza", price: 8 }, { image: "/images/combo-doughnut.jpg", title: "Doughnut", price: 4 }],
+    [{ image: "/images/combo-7up.jpg", title: "7Up", price: 3 }, { image: "/images/combo-popcorn.jpg", title: "Popcorn", price: 5 }, { image: "/images/combo-kitkat.jpg", title: "Kit Kat", price: 4 }]
   ]
 
   return (
@@ -33,16 +40,16 @@ const Experiences = () => {
           transition={{ duration: 1, delay: 3 }}
           className="w-3/4 md:w-1/2"
         >
-          <h1 className="mt-10 mb-5 text-white text-center uppercase text-sm md:text-5xl">Bite-sized adventures</h1>
-          <h1 className="m-10 text-white text-justify text-sm md:text-2xl">Turn your trip to the cinema into a cheerful dining experience with our selection of made-for-cinema bites. Treat yourself to nachos, hot dogs and chicken strips or share popcorn and chocolate-covered treats with your date.</h1>
-          <Carousel className="mb-20" slides={slides} />
+          <h1 className="mt-10 text-white text-center uppercase text-sm md:text-5xl">Bite-sized adventures</h1>
+          <h1 className="m-5 text-white text-justify text-sm md:text-2xl">Turn your trip to the cinema into a cheerful dining experience with our selection of made-for-cinema bites. Treat yourself to nachos, hot dogs and chicken strips or share popcorn and chocolate-covered treats with your date.</h1>
+          <Carousel className="mb-10" slides={slides} />
         </motion.div>
 
         <motion.div
           initial={{ opacity: "0" }}
           animate={{ opacity: "100%" }}
           transition={{ duration: 1, delay: 3 }}
-          className="md:w-3/4 mb-20"
+          className="md:w-3/4 mb-5"
         >
           <h1 className="mb-10 text-white text-center uppercase text-sm md:text-5xl">Your Cinema Favorites</h1>
 
@@ -54,6 +61,19 @@ const Experiences = () => {
             }
           </div>
         </motion.div>
+
+        <div className="m-5 mb-10 md:w-1/2">
+          <h1 className="text-sm md:text-5xl text-white text-center uppercase">Try Our Combo Boxes</h1>
+          {
+            comboBoxes.map((products, i) => (
+              <ComboBox
+                key={i}
+                title={`Combo Box #${i + 1}`}
+                products={products}
+              />
+            ))
+          }
+        </div>
       </div>
     </LayoutPage>
   )
