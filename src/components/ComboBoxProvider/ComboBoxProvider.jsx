13fb36933bfe46b05642a90b07af06005149e2ad
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-05 14:10:37 +0300

    add titles to combo boxes in ComboBoxProvider

--- src/components/ComboBoxProvider/ComboBoxProvider.jsx
+++ src/components/ComboBoxProvider/ComboBoxProvider.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 
 const comboBoxes = {
-  "12wer": { products: [{ image: "/images/combo-coca-cola.jpg", title: "Coca-Cola", price: 3 }, { image: "/images/combo-chips.jpg", title: "Chips", price: 7 }, { image: "/images/combo-ice-cream.jpg", title: "Ice-Cream", price: 10 }] },
-  "23dfg": { products: [{ image: "/images/combo-fanta.jpg", title: "Fanta", price: 3 }, { image: "/images/combo-pizza.jpg", title: "Pizza", price: 8 }, { image: "/images/combo-doughnut.jpg", title: "Doughnut", price: 4 }] },
-  "34sjd": { products: [{ image: "/images/combo-7up.jpg", title: "7Up", price: 3 }, { image: "/images/combo-popcorn.jpg", title: "Popcorn", price: 5 }, { image: "/images/combo-kitkat.jpg", title: "Kit Kat", price: 4 }] }
+  "12wer": { title: "Combo Box #1", products: [{ image: "/images/combo-coca-cola.jpg", title: "Coca-Cola", price: 3 }, { image: "/images/combo-chips.jpg", title: "Chips", price: 7 }, { image: "/images/combo-ice-cream.jpg", title: "Ice-Cream", price: 10 }] },
+  "23dfg": { title: "Combo Box #2", products: [{ image: "/images/combo-fanta.jpg", title: "Fanta", price: 3 }, { image: "/images/combo-pizza.jpg", title: "Pizza", price: 8 }, { image: "/images/combo-doughnut.jpg", title: "Doughnut", price: 4 }] },
+  "34sjd": { title: "Combo Box #3", products: [{ image: "/images/combo-7up.jpg", title: "7Up", price: 3 }, { image: "/images/combo-popcorn.jpg", title: "Popcorn", price: 5 }, { image: "/images/combo-kitkat.jpg", title: "Kit Kat", price: 4 }] }
 }
 
 const ComboBoxProvider = ({ children }) => React.cloneElement(children, { comboBoxes })
