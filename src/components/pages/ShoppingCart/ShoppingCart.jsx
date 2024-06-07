7375f2a6f85a52b54c7a32f142695beaf7ece753
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-07 14:18:01 +0300

    add a link to the experiences page in the header of the ShoppingCart

--- src/components/pages/ShoppingCart/ShoppingCart.jsx
+++ src/components/pages/ShoppingCart/ShoppingCart.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 import persistentShoppingCart from "persistent/persistentShoppingCart"
 
 import ShoppingCartItem from "./ShoppingCartItem"
@@ -19,7 +21,15 @@ const ShoppingCart = ({ comboBoxes }) => {
 
   return (
     <div className="h-screen bg-gray-100">
-      <h1 className="bg-blue-400 text-center text-white p-1">Shopping Cart</h1>
+      <div className="bg-blue-400 text-center text-white p-2 flex justify-center items-center">
+        <Link to="/experiences" className="text-white hover:text-white absolute left-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+          </svg>
+        </Link>
+
+        <div className="text-4xl">Shopping Cart</div>
+      </div>
       <div className="mt-20 flex flex-col md:flex-row justify-center gap-5">
         {
           shoppingCartItems.map(v => (<ShoppingCartItem key={v.id} {...v} />))
--- src/components/pages/ShoppingCart/ShoppingCart.test.jsx
+++ src/components/pages/ShoppingCart/ShoppingCart.test.jsx
@@ -1,9 +1,16 @@
 import { render } from "@testing-library/react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import ShoppingCart from "./ShoppingCart"
 
 test("should render pages/ShoppingCart component", () => {
-  const view = render(<ShoppingCart />)
+  const view = render(
+    <Router>
+      <Routes>
+        <Route path="/" element={<ShoppingCart />} />
+      </Routes>
+    </Router>
+  )
 
   expect(view).toMatchSnapshot()
 })
