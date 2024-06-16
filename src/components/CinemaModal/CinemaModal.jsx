aedef2cb17f0dc13b7fe8ecc9ca56ecc9f39f3a0
Grisha Angelov <grisha.angelov89@gmail.com>
2024-06-16 13:46:01 +0300

    visualize all cinemas in CinemaModal

--- src/components/CinemaModal/CinemaModal.jsx
+++ src/components/CinemaModal/CinemaModal.jsx
@@ -1,3 +1,15 @@
+import Cinema from "./Cinema"
+
+const cinemas = [
+  { id: "21203141377895407", name: "Acton", address: "Royale Leisure Park, Kendal Avenue, London" },
+  { id: "9604801004817922", name: "London Haymarket", address: "11/18 Panton Street, London" },
+  { id: "8498625877351675", name: "London Leicester Square", address: "22-24 Leicester Square, London" },
+  { id: "6127704012543322", name: "London West End", address: "38A Leicester Square , London" },
+  { id: "4513144748187281", name: "Leicester", address: "Freemens Park, 90 Aylestone Road, Leicester" },
+  { id: "2149127537035087", name: "Liverpool Switch Island", address: "Dunningsbridge Road, Bootle, Liverpool" },
+  { id: "3111878685449305", name: "Sheffield", address: "Arundel Gate, Sheffield" }
+]
+
 const CinemaModal = ({ onClose }) => (
   <div className="fixed top-0 bg-black bg-opacity-75 w-screen h-screen flex justify-center items-center">
     <div className="bg-white w-[320px] md:w-3/4 xl:w-1/2">
@@ -9,6 +21,11 @@ const CinemaModal = ({ onClose }) => (
         </svg>
       </div>
       <div className="max-h-[300px] overflow-auto">
+        {
+          cinemas.map(v => (
+            <Cinema key={v.id} {...v} />
+          ))
+        }
       </div>
     </div>
   </div>
